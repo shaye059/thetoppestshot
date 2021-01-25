b6ddf248bc4c177523bed978e1e2b20d44756a85
shaye059 <spencer.hayes.laverdiere@gmail.com>
2021-01-24 19:50:24 -0500

    Testing opening reddit

--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
 
 
-def print_hi(name):
+
+def main():
     # Use a breakpoint in the code line below to debug your script.
+    # Using Chrome to access web
+    driver = webdriver.Chrome()
+    # Open the website
+    driver.get('https://www.reddit.com/')
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # main loop
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
